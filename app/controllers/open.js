9e1dfb92b1e10d5e79181dd842a0b533673e5e3f
zhuangzhaokai <zhaokai.zhuang@gmail.com>
2020-03-18 12:04:18 +0800

    测试pm2重启

--- app/controllers/open.js
+++ app/controllers/open.js
@@ -2,24 +2,27 @@
 const testModel = require('../models/test.server.model')
 module.exports = {
   demo: async (ctx, next) => {
-
-    testModel.create()
-    let testEnity = new testModel({
-      id: 1,
-      name: 'test',
-      count: 1.221
-    })
-
-    // 创建用户
-    testModel.create(testEnity, (err, data) => {
-      if (err) return console.log(err)
-    })
     ctx.body = {
-      status: 200,
-      message: '成功',
-      data: {
-
-      }
+      info: '测试pm2重启'
     }
+
+    // testModel.create()
+    // let testEnity = new testModel({
+    //   id: 1,
+    //   name: 'test',
+    //   count: 1.221
+    // })
+    //
+    // // 创建用户
+    // testModel.create(testEnity, (err, data) => {
+    //   if (err) return console.log(err)
+    // })
+    // ctx.body = {
+    //   status: 200,
+    //   message: '成功',
+    //   data: {
+    //
+    //   }
+    // }
   },
 }
