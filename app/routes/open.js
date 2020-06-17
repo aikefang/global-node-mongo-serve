503037c27174516477d02050aeddc98af3e9e061
zhuangzhaokai <zhaokai.zhuang@gmail.com>
2020-06-17 13:08:29 +0800

    coding

--- app/routes/open.js
+++ app/routes/open.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router') // koa 路由中间件
 const router = Router()
 let controller = require('../controllers/open')
+// const $axios = require('axios')
 const ai = require('../../lib/abnormal-interception')
 router.get('/v1/demo', ai(controller.demo))
 router.get('/v1/feishu', ai(controller.feishu))
