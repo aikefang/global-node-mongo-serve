803517392b8662fd150bb8c69b8fa9adaf6bf1a3
zhuangzhaokai <zhaokai.zhuang@gmail.com>
2020-06-17 12:06:52 +0800

    coding

--- app/controllers/open.js
+++ app/controllers/open.js
@@ -30,7 +30,7 @@ module.exports = {
     // }
   },
   async feishu(ctx) {
-    await axios.post('https://open.feishu.cn/open-apis/bot/hook/d93784d224f9402587c32eb3fe2051c6', {
+    await $axios.post('https://open.feishu.cn/open-apis/bot/hook/d93784d224f9402587c32eb3fe2051c6', {
       title: '订阅消息',
       text: JSON.stringify(ctx.request.body),
     })
--- app/routes/open.js
+++ app/routes/open.js
@@ -4,5 +4,6 @@ let controller = require('../controllers/open')
 const ai = require('../../lib/abnormal-interception')
 router.get('/v1/demo', ai(controller.demo))
 router.get('/v1/feishu', ai(controller.feishu))
+router.post('/v1/feishu', ai(controller.feishu))
 
 module.exports = router
