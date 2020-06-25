5331a9bcc8e318306736ada8850168d1e23cf19e
zhuangzhaokai <github@aikefang.com>
2020-06-26 02:40:43 +0800

    changepwd

--- pm2.config.js
+++ pm2.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "node-mongo-services",                   // 项目名
       script: "./bin/server.js",               // 执行文件
       watch: true,                             // 是否监听文件变动然后重启
+      cwd: "./",
       env_production: {
         "PORT": 2020,
         "NODE_ENV": "production",
