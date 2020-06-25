4694cc5dced5b23b75e270e5572676551b22b179
zhuangzhaokai <github@aikefang.com>
2020-06-26 03:04:26 +0800

    c

--- pm2.config.js
+++ pm2.config.js
@@ -2,9 +2,11 @@ module.exports = {
   apps : [
     {
       name: "node-mongo-services",                   // 项目名
-      script: "./server.js",               // 执行文件
+      script: "server.js",               // 执行文件
       watch: true,                             // 是否监听文件变动然后重启
-      cwd: "./",
+      // cwd: "./",
+      exec_interpreter: "node",
+      "log_date_format": "YYYY-MM-DD HH:mm Z",
       env_production: {
         "PORT": 2020,
         "NODE_ENV": "production",
