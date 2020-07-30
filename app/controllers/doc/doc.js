fd01e96f0e26436e11053a15754698082818f8be
zhuangzhaokai <zhaokai.zhuang@gmail.com>
2020-07-30 10:40:33 +0800

    coding

--- app/controllers/doc/doc.js
+++ app/controllers/doc/doc.js
@@ -4,7 +4,13 @@ const docModel = require('../../models/doc/doc')
 module.exports = {
   async search(ctx) {
     const keyword = ctx.request.query.keyword
-    const reg = new RegExp(keyword, 'i')
+    const pageNum = ctx.request.query.pageNum || 1
+    const pageSize = ctx.request.query.pageSize || 10
+    const checked = common.checkRequired({
+      keyword
+    }, ctx)
+    if (!checked) return
+    // const reg = new RegExp(keyword, 'i')
 
     const kArr = [...new Set(keyword.split(' '))].filter((str) => {
       return str !== ''
@@ -33,6 +39,12 @@ module.exports = {
         title: 1,
       }
     )
+      .sort({
+        views: -1
+      })
+      .skip((pageNum - 1) * pageSize)
+      .limit(pageSize)
+
 
     /**
      *  搜索优先级排序规则
@@ -58,104 +70,34 @@ module.exports = {
         kArr,
       }
     }
+  },
+
+  async content(ctx) {
+    const path = ctx.request.query.path
+
+    const checked = common.checkRequired({
+      path
+    }, ctx)
+    if (!checked) return
+
+    const res = await docModel.findOne({
+      path,
+    }, {
+      commit: 0
+    })
+
+    if (res) {
+      return ctx.body = {
+        status: 200,
+        message: 'ok',
+        data: res
+      }
+    }
+
+    ctx.body = {
+      status: 200004,
+      message: '数据不存在',
+      data: {}
+    }
   }
-  // async create(ctx) {
-  //   const {
-  //     parent,
-  //     title,
-  //     seo,
-  //     content,
-  //     tags
-  //   } = ctx.request.body
-  //
-  //   const checked = common.checkRequired({
-  //     parent,
-  //     title,
-  //     seo,
-  //     content,
-  //   }, ctx)
-  //   if (!checked) {
-  //     return
-  //   }
-  //
-  //   // 检查标签ID是否为合法的ID
-  //   const checkTags = tags.every(data =>  common.ObjectId(data) !== false)
-  //   if (!checkTags) {
-  //     return ctx.body = {
-  //       status: 500003,
-  //       message: '参数错误',
-  //     }
-  //   }
-  //
-  //
-  //
-  //
-  //
-  //   if (parent === 1) { // 父级
-  //
-  //     // 验证父级分类的seo是否存在
-  //     const findSeo = await docModel.findOne({
-  //       seo,
-  //
-  //     })
-  //
-  //
-  //
-  //
-  //     const docEnity = new docModel({
-  //       title,
-  //       seo,
-  //       content,
-  //       author: ctx.session.userInfo._id,
-  //       tags: [
-  //         '5f16c6e80e7ebb2e5463c5c5',
-  //         '5f16c6fd98b1e12e5c107ab2'
-  //       ],
-  //     })
-  //   } else { // 子集
-  //
-  //   }
-  //
-  //   const recordEnity = new docModel({
-  //     title: '文档',
-  //     author: ctx.session.userInfo._id,
-  //     // tags: [
-  //     //   common.ObjectId('5f16c6e80e7ebb2e5463c5c5'),
-  //     //   common.ObjectId('5f16c6fd98b1e12e5c107ab2')
-  //     // ]
-  //     tags: [
-  //       '5f16c6e80e7ebb2e5463c5c5',
-  //       '5f16c6fd98b1e12e5c107ab2'
-  //     ],
-  //     tags2: [
-  //       {
-  //         id: '5f16c6e80e7ebb2e5463c5c5'
-  //       },
-  //       {
-  //         id: '5f16c6fd98b1e12e5c107ab2'
-  //       }
-  //     ]
-  //   })
-  //   const res = await docModel.create(recordEnity).catch(e => console.log(e))
-  //
-  //   if (res) {
-  //     ctx.body = {
-  //       status: 200,
-  //       message: '成功',
-  //       data: {
-  //         ...res.toObject()
-  //       }
-  //     }
-  //   } else {
-  //     ctx.body = {
-  //       status: 500000,
-  //       message: '系统错误',
-  //       data: {}
-  //     }
-  //   }
-  // },
-  // async list(ctx) {
-  //   const res = await docModel.find().populate('tags').populate('tags2.id')
-  //   ctx.body = res
-  // }
 }
--- app/routes/doc/doc.js
+++ app/routes/doc/doc.js
@@ -6,5 +6,6 @@ const ai = require('../../../lib/abnormal-interception')
 // router.post('/create', ai(controller.create))
 // router.get('/list', ai(controller.list))
 router.get('/search', ai(controller.search))
+router.get('/content', ai(controller.content))
 
 module.exports = router
